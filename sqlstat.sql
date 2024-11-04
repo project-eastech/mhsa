c132a566864aeca2cee29d14fa3c1eb0bf887aa9
project-eastech <ellis.chiang@eastech.com.hk>
2024-11-04 18:28:13 +0800

    Update sqlstat.sql

--- sqlstat.sql
+++ sqlstat.sql
@@ -1,9 +1,7 @@
 SELECT 
     'IF OBJECT_ID(''' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ''',''U'') IS NOT NULL DROP EXTERNAL TABLE ' + 
     QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + '; CREATE EXTERNAL TABLE ' + QUOTENAME(s.name) + '.' + QUOTENAME(t.name) + ' (' + 
-    (
-        SELECT 
-            ' ' + QUOTENAME(c.name) + ' ' + ty.name +
+        STRING_AGG(' ' + QUOTENAME(c.name) + ' ' + ty.name + 
             CASE 
                 WHEN ty.name IN ('varchar', 'char', 'nvarchar', 'nchar') THEN 
                     '(' + CAST(c.max_length / CASE WHEN ty.name IN ('nvarchar', 'nchar') THEN 2 ELSE 1 END AS NVARCHAR(10)) + ')'
@@ -16,24 +14,21 @@ SELECT
             CASE 
                 WHEN c.is_nullable = 0 THEN ' NOT NULL'
                 ELSE ' NULL'
-            END + ', '
-        FROM sys.columns AS c
-        JOIN sys.types AS ty ON c.user_type_id = ty.user_type_id
-        WHERE c.object_id = t.object_id
-        ORDER BY c.column_id
-        FOR XML PATH(''), TYPE
-    ).value('.', 'NVARCHAR(MAX)')
-    + ')' + 
+            END, ', ') WITHIN GROUP (ORDER BY c.column_id) + 
+    ')' + 
     ' WITH (' + 
     'LOCATION = ''' + et.location + ''', ' + 
     'DATA_SOURCE = ' + QUOTENAME(REPLACE(REPLACE(eds.name, 'hksynd' , 'hksynp'), 'uat', 'prd')) + ', ' + 
-    'FILE_FORMAT = ' + QUOTENAME(eff.name) + ', REJECT_TYPE = VALUE, REJECT_VALUE = 0);' AS CREATE_EXTERNAL_TABLE_STATEMENT
+    'FILE_FORMAT = ' + QUOTENAME(eff.name) + ', REJECT_TYPE = VALUE, REJECT_VALUE = 0);'
 FROM sys.tables AS t
 JOIN sys.schemas AS s ON t.schema_id = s.schema_id
+JOIN sys.columns AS c ON t.object_id = c.object_id
 JOIN sys.external_tables AS et ON t.object_id = et.object_id
+JOIN sys.types AS ty ON c.user_type_id = ty.user_type_id
 JOIN sys.external_data_sources AS eds ON et.data_source_id = eds.data_source_id
 JOIN sys.external_file_formats AS eff ON et.file_format_id = eff.file_format_id
 WHERE t.is_external = 1
+-- AND t.name LIKE 'ext_CA_FidessaPosting_his_trn_fo_ca_entitlement'
 AND t.name LIKE 'ext_idl_%'
 AND s.name = 'dbo'
 GROUP BY s.name, t.name, et.location, eds.name, eff.name;
